78369083a297d07725259a90e8a4eafea1719a4c
Ankur <arvjunk@gmail.com>
2026-02-24 10:51:06 -0500

    Hero Component Scrolling

--- src/app/features/home/hero/hero.component.ts
+++ src/app/features/home/hero/hero.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
+  imports: [MatIconModule]
 })
-export class HeroComponent {}
+export class HeroComponent {
+  /** Id of the element to scroll to when the chevron is clicked. */
+  scrollTargetId = input<string>('home-content');
+
+  scrollToContent(): void {
+    const id = this.scrollTargetId();
+    const el = document.getElementById(id);
+    el?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
