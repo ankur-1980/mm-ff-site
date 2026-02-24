6a6a785dc480568e85b329af5356e93354a0a1ee
Ankur <arvjunk@gmail.com>
2026-02-24 10:43:41 -0500

    Hamburger menu on mobile
    - < 768px

--- src/app/shell/app-shell.component.ts
+++ src/app/shell/app-shell.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-shell',
   imports: [
+    MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
+    MatIconModule,
     RouterLink,
     RouterLinkActive,
     RouterOutlet
@@ -15,5 +19,14 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app-shell.component.html',
   styleUrl: './app-shell.component.scss'
 })
-export class AppShellComponent {}
+export class AppShellComponent {
+  @ViewChild(MatSidenav) drawer!: MatSidenav;
 
+  toggleDrawer(): void {
+    this.drawer?.toggle();
+  }
+
+  closeDrawerAfterNav(): void {
+    this.drawer?.close();
+  }
+}
