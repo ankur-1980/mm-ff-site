c839ac5be870a5eea2534447aaf1319187173031
Ankur <arvjunk@gmail.com>
2026-02-24 10:39:11 -0500

    Scaffold Hero Component
    - Mobile first layout

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 
 import { LeagueDataService } from '../../data';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-home-page',
+  imports: [HeroComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
