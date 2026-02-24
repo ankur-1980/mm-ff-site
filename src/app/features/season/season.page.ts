3eed4dbf28939d71d3710b752ab7f26c8eb44865
Ankur <arvjunk@gmail.com>
2026-02-24 15:46:53 -0500

    season routes

--- src/app/features/season/season.page.ts
+++ src/app/features/season/season.page.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { SeasonSelector } from './season-selector/season-selector';
 
 @Component({
   selector: 'app-season-page',
+  imports: [SeasonSelector, RouterOutlet],
   templateUrl: './season.page.html',
   styleUrl: './season.page.scss'
 })
