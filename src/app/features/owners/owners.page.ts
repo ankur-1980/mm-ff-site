f55036cc30dafb78af70758e71263cad66a16797
Ankur <arvjunk@gmail.com>
2026-02-27 15:35:30 -0500

    Implement cards on owner page

--- src/app/features/owners/owners.page.ts
+++ src/app/features/owners/owners.page.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { SectionHeader } from '../../shared/components/section-header/section-header';
+import { OwnerProfileCard } from '../../shared/components/owner-profile-card/owner-profile-card';
 import { OWNER_TABS } from './owners-tabs';
 
 @Component({
   selector: 'app-owners-page',
-  imports: [SectionHeader, RouterLink],
+  imports: [SectionHeader, RouterLink, OwnerProfileCard],
   templateUrl: './owners.page.html',
   styleUrl: './owners.page.scss'
 })
