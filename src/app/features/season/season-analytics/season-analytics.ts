ee0e14b59efd83833e9adcb145e44b1655321d49
Ankur <arvjunk@gmail.com>
2026-03-01 15:15:27 -0500

    analytics sidebar styles. subsection header

--- src/app/features/season/season-analytics/season-analytics.ts
+++ src/app/features/season/season-analytics/season-analytics.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { AnalyticsSidebar } from './analytics-sidebar/analytics-sidebar';
+import { SubsectionHeader } from '../../../shared/components/subsection-header/subsection-header';
 
 @Component({
   selector: 'app-season-analytics',
-  imports: [AnalyticsSidebar],
+  imports: [AnalyticsSidebar, SubsectionHeader],
   templateUrl: './season-analytics.html',
   styleUrl: './season-analytics.scss',
 })
